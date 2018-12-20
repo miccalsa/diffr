ed3b9b4b686e7a40e4887122167086ab6ddba477
Mike Calderon <miccalsa@gmail.com>
2018-12-19 23:59:53 -0600

    Add initial logic for comparing resources

--- src/main/java/com/miccalsa/diffr/repository/DiffrRepository.java
+++ src/main/java/com/miccalsa/diffr/repository/DiffrRepository.java
@@ -1,7 +1,5 @@
 package com.miccalsa.diffr.repository;
 
-import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
 
 import com.miccalsa.diffr.domain.DiffrData;
@@ -9,5 +7,5 @@ import com.miccalsa.diffr.dto.DiffrSide;
 
 public interface DiffrRepository extends CrudRepository<DiffrData, Integer> {
 
-    List<DiffrData> findByDiffrIdAndDiffrSide(Integer diffrId, DiffrSide diffrSide);
+    DiffrData findByDiffrIdAndDiffrSide(Integer diffrId, DiffrSide diffrSide);
 }
--- src/main/java/com/miccalsa/diffr/service/impl/DiffrServiceImpl.java
+++ src/main/java/com/miccalsa/diffr/service/impl/DiffrServiceImpl.java
@@ -1,7 +1,6 @@
 package com.miccalsa.diffr.service.impl;
 
-import java.util.List;
-
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,8 +19,8 @@ public class DiffrServiceImpl implements DiffrService {
     @Override
     public void saveResource(Integer id, String source, DiffrSide side) {
         DiffrData data;
-        List<DiffrData> dataList = this.diffrRepository.findByDiffrIdAndDiffrSide(id, side);
-        data = dataList.isEmpty() ? new DiffrData() : dataList.get(0);
+        data = this.diffrRepository.findByDiffrIdAndDiffrSide(id, side);
+        data = data == null ? new DiffrData() : data;
         data.setDiffrId(id);
         data.setData(source);
         data.setDiffrSide(side);
@@ -30,6 +29,27 @@ public class DiffrServiceImpl implements DiffrService {
 
     @Override
     public DiffrResultDto getDiffr(Integer id) {
-        return null;
+        DiffrResultDto resultDto = new DiffrResultDto();
+        DiffrData left = this.diffrRepository.findByDiffrIdAndDiffrSide(id, DiffrSide.LEFT);
+        DiffrData right = this.diffrRepository.findByDiffrIdAndDiffrSide(id, DiffrSide.RIGHT);
+        // TODO: Validate both fields are present
+        byte[] leftBytes = this.decodeResource(left.getData());
+        byte[] rightBytes = this.decodeResource(right.getData());
+
+        if(leftBytes.length == rightBytes.length) {
+            resultDto.setResult("Both resources have same length");
+            checkDifferences();
+        } else {
+            resultDto.setResult("Resources length does not match");
+        }
+
+        return resultDto;
+    }
+
+    private byte[] decodeResource(String resource) {
+        return Base64.decodeBase64(resource.getBytes());
+    }
+
+    private void checkDifferences() {
     }
 }
